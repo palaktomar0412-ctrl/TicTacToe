76fef12edc260359cda700eef277aa6e8c459bc5
Palak Tomar <palaktomar0412@gmail.com>
2026-05-02 18:23:57 +0530

    UC4

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,10 +5,12 @@ public class TicTacToe {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter slot (1-9): ");
         int slot = sc.nextInt();
 
-        System.out.println("You entered: " + slot);
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+
+        System.out.println("Row: " + row + ", Column: " + col);
     }
 }
