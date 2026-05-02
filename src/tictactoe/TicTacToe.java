310f899ca090d99b08c1fa78e8472963934f8432
Palak Tomar <palaktomar0412@gmail.com>
2026-05-02 18:31:35 +0530

    UC5

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,16 +1,39 @@
 package tictactoe;
+
 import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        char[][] board = {
+                {'X', '-', '-'},
+                {'-', '-', '-'},
+                {'-', '-', '-'}
+        };
+
         System.out.print("Enter slot (1-9): ");
         int slot = sc.nextInt();
 
         int row = (slot - 1) / 3;
         int col = (slot - 1) % 3;
 
-        System.out.println("Row: " + row + ", Column: " + col);
+        if (board[row][col] == '-') {
+            board[row][col] = 'O';
+            System.out.println("Move placed.");
+        } else {
+            System.out.println("Already occupied.");
+        }
+
+        System.out.println("Board now:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
